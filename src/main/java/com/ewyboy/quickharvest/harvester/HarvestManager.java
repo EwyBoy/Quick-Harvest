1f9240c1815820d95324786c090859187673fbee
Luke Gilfoyle <boo122333@gmail.com>
2019-12-21 17:23:05 +0000

    Refactor HarvestManager to use Supplier as key
    This change reduces repeated entries in the registry.
    
    Signed-off-by: Luke Gilfoyle <boo122333@gmail.com>

--- src/main/java/com/ewyboy/quickharvest/harvester/HarvestManager.java
+++ src/main/java/com/ewyboy/quickharvest/harvester/HarvestManager.java
@@ -4,43 +4,55 @@ import net.minecraft.block.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class HarvestManager {
 
-    public static final Map<Block, IHarvestable> HARVEST_HANDLER_MAP = new HashMap<>();
+    public static final Map<Predicate<Block>, IHarvestable> HARVEST_HANDLER_MAP = new HashMap<>();
 
+    //TODO: Create a register method and make the harvest handler map private
     static {
-        // TODO maybe move the HARVEST_HANDLER_MAP to use a Predicate<Block> to remove duplicate entries
         // Default plant handlers
-        HARVEST_HANDLER_MAP.put(Blocks.WHEAT, new DefaultHarvestHandler(CropsBlock.AGE));
-        HARVEST_HANDLER_MAP.put(Blocks.CARROTS, new DefaultHarvestHandler(CarrotBlock.AGE));
-        HARVEST_HANDLER_MAP.put(Blocks.POTATOES, new DefaultHarvestHandler(PotatoBlock.AGE));
-        HARVEST_HANDLER_MAP.put(Blocks.BEETROOTS, new DefaultHarvestHandler(BeetrootBlock.BEETROOT_AGE));
-        HARVEST_HANDLER_MAP.put(Blocks.NETHER_WART, new DefaultHarvestHandler(NetherWartBlock.AGE));
-        HARVEST_HANDLER_MAP.put(Blocks.COCOA, new DefaultHarvestHandler(CocoaBlock.AGE));
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.WHEAT), new DefaultHarvestHandler(CropsBlock.AGE));
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.CARROTS), new DefaultHarvestHandler(CarrotBlock.AGE));
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.POTATOES), new DefaultHarvestHandler(PotatoBlock.AGE));
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.BEETROOTS), new DefaultHarvestHandler(BeetrootBlock.BEETROOT_AGE));
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.NETHER_WART), new DefaultHarvestHandler(NetherWartBlock.AGE));
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.COCOA), new DefaultHarvestHandler(CocoaBlock.AGE));
 
         // Stem plant handlers
-        HARVEST_HANDLER_MAP.put(Blocks.ATTACHED_MELON_STEM, new StemPlantHarvestHandler());
-        HARVEST_HANDLER_MAP.put(Blocks.MELON, new StemPlantHarvestHandler());
-        HARVEST_HANDLER_MAP.put(Blocks.ATTACHED_PUMPKIN_STEM, new StemPlantHarvestHandler());
-        HARVEST_HANDLER_MAP.put(Blocks.PUMPKIN, new StemPlantHarvestHandler());
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.ATTACHED_MELON_STEM, Blocks.MELON), new StemPlantHarvestHandler());
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.ATTACHED_PUMPKIN_STEM, Blocks.PUMPKIN), new StemPlantHarvestHandler());
 
         // Tall plant handlers
-        HARVEST_HANDLER_MAP.put(Blocks.SUGAR_CANE, new TallPlantHarvestHandler());
-        HARVEST_HANDLER_MAP.put(Blocks.CACTUS, new TallPlantHarvestHandler());
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.SUGAR_CANE, Blocks.CACTUS), new TallPlantHarvestHandler());
 
         // Chorus plant handler
-        HARVEST_HANDLER_MAP.put(Blocks.CHORUS_FLOWER, new ChorusHarvestManager());
-        HARVEST_HANDLER_MAP.put(Blocks.CHORUS_PLANT, new ChorusHarvestManager());
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.CHORUS_FLOWER, Blocks.CHORUS_PLANT), new ChorusHarvestManager());
 
-        // TODO Huge Mushrooms
-        HARVEST_HANDLER_MAP.put(Blocks.BROWN_MUSHROOM_BLOCK, new HugeMushroomHarvestHandler());
-        HARVEST_HANDLER_MAP.put(Blocks.RED_MUSHROOM_BLOCK, new HugeMushroomHarvestHandler());
-        HARVEST_HANDLER_MAP.put(Blocks.MUSHROOM_STEM, new HugeMushroomHarvestHandler());
+        // Huge mushroom handler
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.BROWN_MUSHROOM_BLOCK, Blocks.RED_MUSHROOM_BLOCK, Blocks.MUSHROOM_STEM), new HugeMushroomHarvestHandler());
 
         // Kelp plant handler
-        HARVEST_HANDLER_MAP.put(Blocks.KELP, new KelpHarvestHandler());
-        HARVEST_HANDLER_MAP.put(Blocks.KELP_PLANT, new KelpHarvestHandler());
+        HARVEST_HANDLER_MAP.put(new MatchBlockPredicate(Blocks.KELP, Blocks.KELP_PLANT), new KelpHarvestHandler());
+    }
+
+    static class MatchBlockPredicate implements Predicate<Block> {
+        private final Block[] blocks;
+
+        MatchBlockPredicate(Block... blocks) {
+            this.blocks = blocks;
+        }
+
+        @Override
+        public boolean test(Block block) {
+            for (Block t : blocks) {
+                if (t == block) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
 }
